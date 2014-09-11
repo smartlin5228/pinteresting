1dc6ef53547199d8c5ba55a53ba50d6a8c40be27
smartlin5228 <smartlin1@gmail.com>
2014-09-11 16:21:05 -0700

    Add pins/user association

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,6 +1,6 @@
 class PinsController < ApplicationController
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
-
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
     @pins = Pin.all
@@ -10,49 +10,43 @@ class PinsController < ApplicationController
   end
 
   def new
-    @pin = Pin.new
+    @pin = current_user.pin.build
   end
 
   def edit
   end
 
   def create
-    @pin = Pin.new(pin_params)
-
-
-      if @pin.save
-        format.html  redirect_to @pin, notice: 'Pin was successfully created.' 
-      else
-        format.html  render :new 
-      end
+    @pin = urrent_user.pin.build(pin_params)
+    if @pin.save
+      redirect_to @pin, notice: 'Pin was successfully created.'
+    else
+      render action: 'new'
+    end
   end
 
 
   def update
-    respond_to do |format|
-      if @pin.update(pin_params)
-        format.html { redirect_to @pin, notice: 'Pin was successfully updated.' }
-        format.json { render :show, status: :ok, location: @pin }
-      else
-        format.html { render :edit }
-        format.json { render json: @pin.errors, status: :unprocessable_entity }
-      end
+    if @pin.update(pin_params)
+      redirect_to @pin, notice: 'Pin was successfully updated.'
+    else
+      render action: 'edit'
     end
   end
 
   def destroy
     @pin.destroy
-    respond_to do |format|
-      format.html { redirect_to pins_url, notice: 'Pin was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to pins_url
   end
 
   private
     def set_pin
       @pin = Pin.find(params[:id])
     end
-
+    def correct_user
+      @pin = current_user.pins.find(params[:id])
+      redirect_to pins_path, notice: "Not Authorized to access this pin" if @pin.nil?
+    end
     def pin_params
       params.require(:pin).permit(:description)
     end
--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,2 +1,6 @@
 class Pin < ActiveRecord::Base
+	belongs_to :user
+
+	validates :image, presence: true
+	validates :description, presence: true
 end
